b46c3224f0bd0ede3deed7a31e3025d4f8fe12c5
walternknBBD <159465313+walternknBBD@users.noreply.github.com>
2024-07-02 17:44:48 +0200

    Update V002__create_crs_payment_table.sql

--- database/migrations/V002__create_crs_payment_table.sql
+++ database/migrations/V002__create_crs_payment_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE crspayment (
-    payment_id SERIAL,
+    payment_id SERIAL PRIMARY KEY,
     monthly_profit MONEY NOT NULL,
     tax_payed MONEY NOT NULL,
     payment_date VARCHAR(10) NOT NULL
