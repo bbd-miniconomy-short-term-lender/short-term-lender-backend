c26d6ff6efab99c9268b0703e15a0e9eecd657a4
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-07-03 15:16:48 +0200

    update loan endpoints

--- src/api/repository/CommercialBankRepository.ts
+++ src/api/repository/CommercialBankRepository.ts
@@ -1,6 +1,6 @@
 import * as https from 'https';
 import { getOptions } from '../../utils';
-import { LoanApplication } from '../../types/commercial-bank';
+import { AccountBalanceResponse, LoanApplication } from '../../types/commercial-bank';
 
 const fetch = require('node-fetch')
 
@@ -8,8 +8,7 @@ export class CommercialBankRepository {
     private COMMERCIAL_BANK_BASE_URL = 'https://api.commercialbank.projects.bbdgrad.com';
 
     async requestLoan(body: LoanApplication) {
-        // confirm port
-        const options = getOptions(this.COMMERCIAL_BANK_BASE_URL, '/loans/apply', 3000, 'POST');
+        const options = getOptions(this.COMMERCIAL_BANK_BASE_URL, '/loans/apply', 'POST');
 
         const agent = new https.Agent({
             rejectUnauthorized: options.rejectUnauthorized,
@@ -17,7 +16,20 @@ export class CommercialBankRepository {
             cert: options.cert
         });
 
-        const res = await fetch(`${options.hostname}/loans/apply`, {agent: agent, method: options.method, body: JSON.stringify(body)});
+        const res = await fetch(`${options.hostname}${options.path}`, {agent: agent, method: options.method, body: JSON.stringify(body)});
         return res.json();
+    }
+
+    async getAccountBalance(): Promise<AccountBalanceResponse> {
+        const options = getOptions(this.COMMERCIAL_BANK_BASE_URL, '/account/balance', 'GET');
+
+        const agent = new https.Agent({
+            rejectUnauthorized: options.rejectUnauthorized,
+            key: options.key,
+            cert: options.cert
+        });
+
+        const res = await fetch(`${options.hostname}${options.path}`, {agent: agent, method: options.method});
+        return res.json() as AccountBalanceResponse;
     }
 }
--- src/types/commercial-bank.ts
+++ src/types/commercial-bank.ts
@@ -1,5 +1,14 @@
 export type LoanApplication = {
-        amount: number;
-        accountName: string;
-        type: string;
+  amount: number;
+  accountName: string;
+  type: string;
+}
+
+export type AccountBalanceResponse = {
+  status: number,
+  data: {
+    accountName: string,
+    accountBalance: number
+  },
+  message: string
 }
