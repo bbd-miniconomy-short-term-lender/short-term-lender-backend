fd3ed37344e76bef82db739c3870c9a632e91818
tristanphips <tristan.phipson@bbd.co.za>
2024-07-03 23:56:09 +0200

    authy

--- src/api/handlers/management-handler.ts
+++ src/api/handlers/management-handler.ts
@@ -6,6 +6,7 @@ export const itsThePurge = async (req: Request, res: Response) => {
     try {
         const loanRepository = new LoanRepository(pool);
         await loanRepository.prugeBabyPurge();
+        console.log('Someone called purge...');
         res.status(200).json({ message: "The purge has begun... eerie screams can be heard off in the distance..." });
     } catch (error) {
         res.status(500).json({ message: "Internal server error - so sorry Sam." });
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { loanRoutes } from "./api/routes/loan-routes";
 import { managementRoutes } from "./api/routes/management-routes";
 import { feRouter } from "./api/routes/fe-routes";
 import cors from "cors";
+import authMiddleware from "./middleware/authMiddleware";
 
 const app: Application = express();
 const PORT: number = 5000;
@@ -12,7 +13,8 @@ const PORT: number = 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-app.use("/", loanRoutes, managementRoutes, feRouter);
+app.use("/", loanRoutes, managementRoutes);
+app.use("/", authMiddleware, feRouter);
 
 // health check
 app.get("/health", (req: Request, res: Response) => {
