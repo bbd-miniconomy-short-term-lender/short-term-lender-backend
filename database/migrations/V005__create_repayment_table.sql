242e3973aa5a87d15823a8acbfb29e5a5c251c22
mantr002 <rotenda.mantsha@vcontractor.co.za>
2024-07-03 12:04:11 +0200

    Database Structure changes

--- database/migrations/V005__create_repayment_table.sql
+++ database/migrations/V005__create_repayment_table.sql
@@ -1,5 +1,13 @@
+-- CREATE TABLE repayment (
+--     repayment_id SERIAL,
+--     amount MONEY NOT NULL
+-- );
+
 CREATE TABLE repayment (
-    repayment_id SERIAL,
-    amount MONEY NOT NULL,
-    loan_status VARCHAR(20) NOT NULL
+    repayment_id SERIAL PRIMARY KEY,
+    loan_id INT NOT NULL,
+    repayment_date DATE NOT NULL,
+    persona_id INT,
+    FOREIGN KEY (persona_id) REFERENCES persona(persona_id)
+    FOREIGN KEY (loan_id) REFERENCES loan_table(loan_id)
 );
