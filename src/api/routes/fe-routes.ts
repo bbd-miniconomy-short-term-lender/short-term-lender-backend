a81ee164f179932111a0a47acf397d9d90047dd9
Tristan Phipson <tristan.phipson@bbd.co.za>
2024-07-03 16:54:51 +0200

    get repayments by id

--- src/api/routes/fe-routes.ts
+++ src/api/routes/fe-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { feGetLoanById, feGetLoanTable, feUpdateLoanStatus } from "../handlers/fe-handler";
+import { feGetLoanById, feGetLoanTable, feGetRepaymentsById, feUpdateLoanStatus } from "../handlers/fe-handler";
 
 
 const router = Router();
 
 router.get("/fe/loan/info/:loanId", feGetLoanById);
 router.get("/fe/loan/table", feGetLoanTable);
+router.get("/fe/repayments/:loanId", feGetRepaymentsById);
 
 router.post("/fe/loan/status", feUpdateLoanStatus);
 
