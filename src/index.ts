031fbf44bc50cd7dfae474a79237bc0f098b2b4f
tristanphips <tristan.phipson@bbd.co.za>
2024-07-03 22:31:20 +0200

    Пугать ежа голой жопой.

--- src/index.ts
+++ src/index.ts
@@ -17,38 +17,9 @@ app.use("/", loanRoutes, managementRoutes, feRouter);
 // health check
 app.get("/health", (req: Request, res: Response) => {
     res.send({
-        message: 'I AM ALIVE BABY!',
-        shrek: `
-            Donkey: Shrek, what are you doing?
-            Shrek: I just..., you know...
-            Donkey: Oh, come on, I was just kidding. And that one, that's Throwback. The only Ogre to ever spit over three wheat fields.
-            Shrek: Right. Yeah.
-            Donkey: Hey, can you tell my future from these stars?
-            Shrek: Well, the stars don't tell the future, Donkey. They tell stories.
-                c,_.--.,y
-                  7 a.a(
-                (   ,_Y)
-                :  '---;
-             ___.'\\\\.  - (
-            .'\"\"\"S,._'--'_2..,_
-            |    ':::::=:::::  \\
-            .     f== ;-,---.' T
-            Y.   r,-,_/_      |
-            |:\\___.---' '---./
-            |'\\\`             )
-            \\\\             ,
-            ':;,.________.;L
-            /  '---------' |
-            |              \\\\
-            L---'-,--.-'--,-'
-            T    /   \\\\   Y
-            |   Y    ,   |
-            |   \\\\    (   |
-            (   )     \\\\,_L
-            7-./      )  \`,
-            /  _(      '._  \\\\
-            '---'           '--'
-        `
+        message: 'Server status: Up and running!',
+        quote: 'Пугать ежа голой жопой. ',
+        uptime: process.uptime()
     })
 });
 
