79436a634bbbe5df6896fc8ad230e055b4cf5c08
tristanphips <tristan.phipson@bbd.co.za>
2024-07-03 22:20:59 +0200

    whys the rum gone?

--- src/api/queries/loan-queries.ts
+++ src/api/queries/loan-queries.ts
@@ -2,3 +2,5 @@ export const addLoan = "INSERT INTO loan (persona_id, amount, interest_rate, loa
 export const getAllLoans = "SELECT * FROM loan"
 export const getLoanById = "SELECT * FROM loan WHERE loan_id = ($1)"
 export const getLastLoanRecords = "SELECT * FROM loan ORDER BY loan_id DESC LIMIT $1";
+export const updateStatusByLoan = "UPDATE loan SET loan_status = ($2) WHERE loan_id = ($1)";
+export const checkLoanExists = "SELECT EXISTS(SELECT 1 FROM loan WHERE loan_id = ($1))";
--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,41 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use("/", loanRoutes, managementRoutes, feRouter);
 
 // health check
-app.get("/ping", (req: Request, res: Response) => {
-    res.send({message: 'pong'})
+app.get("/health", (req: Request, res: Response) => {
+    res.send({
+        message: 'I AM ALIVE BABY!',
+        shrek: `
+            Donkey: Shrek, what are you doing?
+            Shrek: I just..., you know...
+            Donkey: Oh, come on, I was just kidding. And that one, that's Throwback. The only Ogre to ever spit over three wheat fields.
+            Shrek: Right. Yeah.
+            Donkey: Hey, can you tell my future from these stars?
+            Shrek: Well, the stars don't tell the future, Donkey. They tell stories.
+                c,_.--.,y
+                  7 a.a(
+                (   ,_Y)
+                :  '---;
+             ___.'\\\\.  - (
+            .'\"\"\"S,._'--'_2..,_
+            |    ':::::=:::::  \\
+            .     f== ;-,---.' T
+            Y.   r,-,_/_      |
+            |:\\___.---' '---./
+            |'\\\`             )
+            \\\\             ,
+            ':;,.________.;L
+            /  '---------' |
+            |              \\\\
+            L---'-,--.-'--,-'
+            T    /   \\\\   Y
+            |   Y    ,   |
+            |   \\\\    (   |
+            (   )     \\\\,_L
+            7-./      )  \`,
+            /  _(      '._  \\\\
+            '---'           '--'
+        `
+    })
 });
 
 app.listen(PORT, () => {
